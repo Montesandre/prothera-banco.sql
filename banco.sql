5b6216b8b55f6d3045212a3ac615b7534d9a0135
André <montesandre@outlook.com>
2025-04-08 15:45:31 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -3,24 +3,24 @@ SELECT *
 FROM pessoas
 ORDER BY nome ASC;
 
--- 👨‍🦱👩 Pessoas com idade entre 20 e 60 anos
+-- Pessoas com idade entre 20 e 60 anos
 SELECT * 
 FROM pessoas
 WHERE idade BETWEEN 20 AND 60;
 
--- 🕵️‍♀️ Pessoas que não possuem CPF
+--  Pessoas que não possuem CPF
 -- Aqui estamos procurando pessoas que não têm um documento do tipo CPF
 SELECT p.* 
 FROM pessoas p
 LEFT JOIN documentos d ON p.id = d.pessoa_id AND d.tipo = 'CPF'
 WHERE d.tipo IS NULL;
 
--- 🧬 Pessoas com sobrenome 'Soares'
+--  Pessoas com sobrenome 'Soares'
 -- Aqui usamos o LIKE pra pegar qualquer pessoa cujo nome termina ou contém 'Soares'
 SELECT * 
 FROM pessoas
 WHERE nome LIKE '%Soares%';
 
--- 📄 Listar todos os tipos de documentos (sem repetir)
+--  Listar todos os tipos de documentos (sem repetir)
 SELECT DISTINCT tipo 
 FROM documentos;
